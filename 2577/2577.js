64cdaf9c6d52cb29d2d71716f6b1fa6416c3ec91
Seung-wan <tmddhks0104@gmail.com>
2022-05-02 23:02:19 +0900

    2577

--- 2577/2577.js
+++ 2577/2577.js
@@ -1,16 +1,16 @@
 let fs = require('fs');
 let input = fs.readFileSync('./input.txt').toString().split('\n');
-input = input.map((item) => Number(item));
+
 const mulNumber = input.reduce((acc, cur) => acc * cur).toString();
 
-const array = [];
+const obj = {};
+
 for (let i = 0; i < 10; i++) {
-  let cnt = 0;
-  for (let j = 0; j < mulNumber.length; j++) {
-    if (mulNumber[j] === i.toString()) cnt++;
-  }
-  array.push(cnt);
+  obj[i] = 0;
 }
-array.forEach((item) => {
-  console.log(+item);
-});
+
+for (let number of mulNumber) {
+  obj[number] += 1;
+}
+
+for (let value of Object.values(obj)) console.log(value);
