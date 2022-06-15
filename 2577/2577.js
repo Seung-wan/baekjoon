1887c0f3a993b12a4fc27756076f9efdbf9abf9a
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-15 23:22:09 +0900

    add 2577

--- 2577/2577.js
+++ 2577/2577.js
@@ -1,16 +1,16 @@
-let fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().split('\n');
+const fs = require('fs')
+const input = fs.readFileSync('./input.txt').toString().split('\n')
 
-const mulNumber = input.reduce((acc, cur) => acc * cur).toString();
+const mulNumber = String(input[0] * input[1] * input[2])
 
-const obj = {};
+const obj = {}
 
 for (let i = 0; i < 10; i++) {
-  obj[i] = 0;
+  obj[i] = 0
 }
 
 for (let number of mulNumber) {
-  obj[number] += 1;
+  obj[Number(number)] += 1
 }
 
-for (let value of Object.values(obj)) console.log(value);
+for (let value of Object.values(obj)) console.log(value)
