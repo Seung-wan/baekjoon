9945b087bb4e1f9b806dea354d0c5639f85ded7f
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-17 11:37:49 +0900

    add 11654

---
+++ 11654/11654.js
@@ -0,0 +1,8 @@
+let fs = require('fs')
+let input = fs.readFileSync('input.txt').toString() //.split('\n');
+console.log(input.charCodeAt(0))
+
+/**
+ * 백준 제출시 readFIleSync 경로 변경
+ * /dev/stdin
+ */
