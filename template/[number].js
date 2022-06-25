50001cd4198cc2735e59c06cf2cfd747cf87737c
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-25 23:48:18 +0900

    update template (let -> const)

--- template/[number].js
+++ template/[number].js
@@ -1,6 +1,6 @@
-let fs = require('fs')
-let input = fs.readFileSync('input.txt').toString() //.split('\n');
-input = input.map((item) => Number(item))
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString(); //.split('\n');
+input = input.map((item) => Number(item));
 
 /**
  * 백준 제출시 readFIleSync 경로 변경
